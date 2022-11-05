fbb2c7f107b7ac259b8d0879f1b380c58606f8e3
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-11-05 12:26:05 +0100

    styling

--- src/Qrcode.js
+++ src/Qrcode.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import QRCode from 'react-qr-code';
+import './qrcode.css';
+import { AiOutlineQrcode } from 'react-icons/ai';
 
 function Qrcode () {
     const [ value, setValue ] = useState();
@@ -9,49 +11,57 @@ function Qrcode () {
 
   return (
     <div className="qrcode">
-      <center>
-        <br />
-        <br />
-        <input
-          type="text"
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Value of Qrcode"
-        />
-        <br />
-        <br />
-        <input
-          type="text"
-          onChange={(e) => setBack(e.target.value)}
-          placeholder="Background color"
-        />
-        <br />
-        <br />
-        <input
-          type="text"
-          onChange={(e) => setFore(e.target.value)}
-          placeholder="Foreground color"
-        />
-        <br />
-        <br />
-        <input
-          type="number"
-          onChange={(e) =>
-            setSize(parseInt(e.target.value === "" ? 0 : e.target.value, 10))
-          }
-          placeholder="Give Size of Qr-code"
-        />
-        <br />
-        <br />
-        {value && (
-        <QRCode
-        title="iBukunMedia"
-            value={ value }
-            bgColor={ back }
-            fgColor={ fore }
-            size={ size === "" ? 0 : size }
-        />
-        ) }
-      </center>
+      <div className="header">
+        <div className="brand">iBukun</div>
+      </div>
+      <div className="code-elements">
+        <div className="code-card">
+          <h1>
+            <AiOutlineQrcode/> 
+            QRCode Generator
+          </h1>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Value of Qrcode"
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setBack(e.target.value)}
+            placeholder="Background color"
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setFore(e.target.value)}
+            placeholder="Foreground color"
+          />
+          <br />
+          <br />
+          <input
+            type="number"
+            onChange={(e) =>
+              setSize(parseInt(e.target.value === "" ? 0 : e.target.value, 10))
+            }
+            placeholder="Give Size of Qr-code"
+          />
+          <br />
+          <br />
+          {value && (
+            <QRCode
+              title="iBukunMedia"
+              value={value}
+              bgColor={back}
+              fgColor={fore}
+              size={size === "" ? 0 : size}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
